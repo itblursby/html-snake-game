8b84ae32b185034fd21330c7c876eeee96e1f688
itblursby <morriswu65@gmail.com>
2024-08-18 19:27:12 -0700

    add basic functionality and also basic renderer

---
+++ src/MainRenderer.js
@@ -0,0 +1,19 @@
+export const bgCanvasElm = document.getElementById("bgCanvas");
+bgCanvasElm.width = 320;
+bgCanvasElm.height = 240;
+export const bgCanvasCtx = bgCanvasElm.getContext("2d");
+
+export const gameCanvasElm = document.getElementById("gameCanvas");
+gameCanvasElm.width = 320;
+gameCanvasElm.height = 240;
+export const gameCanvasCtx = gameCanvasElm.getContext("2d");
+
+export const uiCanvasElm = document.getElementById("UICanvas");
+uiCanvasElm.width = 320;
+uiCanvasElm.height = 240;
+export const uiCanvasCtx = uiCanvasElm.getContext("2d");
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,39 @@
-import { renderBackground } from "./renderer.js";
+import { renderBackground } from "./BackgroundRenderer.js";
+
+import { renderSnake } from "./SnakeTilesetRenderer.js";
 window.onload = init;
 
-function init() {
-    renderBackground();
+
+const keyActions = {
+    "ArrowRight" : () => { moveSnake(1,0) },
+    "ArrowLeft" : () => { moveSnake(-1,0) },
+    "ArrowDown" : () => { moveSnake(0,1) },
+    "ArrowUp" : () => { moveSnake(0,-1) },
 }
+
+
+
+const snake = [[0,0], [1,0], [2,0], [3,0], [4,0], [5,0]];
+
+function moveSnake(dx, dy) {
+    const head = snake[snake.length-1];
+    snake.push([head[0] + dx, head[1] + dy]);
+    snake.shift();
+    console.log(snake);
+}
+
+function init() {
+
+    renderBackground();
+
+    renderSnake(snake);
+    addEventListener("keydown", (event) => {keyDown(event)});
+}
+
+function keyDown(event) {
+    if (Object.keys(keyActions).includes(event.key)) {
+        keyActions[event.key]();
+        renderSnake(snake);
+    }
+}
+
